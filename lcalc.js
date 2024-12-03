f2a1dff099224213ec9365415a1e8d4d6b66fc71
wigguus <147972060+wigguus@users.noreply.github.com>
2024-12-03 11:35:08 +0100

    Update lcalc.js

--- lcalc.js
+++ lcalc.js
@@ -39,6 +39,7 @@ function onHeartClicked() {
                 "pawciołek",
                 "pawlonek", 
                 "paweł",
+                "pawel",
                 "virkyu",
                 "wiggy",
                 "wigguś",
