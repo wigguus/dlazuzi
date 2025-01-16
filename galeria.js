24c557aa8c5e8bb3f9ace89406beb3ed031c333f
wigguus <147972060+wigguus@users.noreply.github.com>
2025-01-16 10:31:18 +0100

    Final gallery fix

--- galeria.js
+++ galeria.js
@@ -1,17 +1,33 @@
 let actualImage = 1;
+let blockScrolling = false;
 
 function plusImage() {
-    if (actualImage > 0 && actualImage < 7) {
-        actualImage += 1;
-        window.location.href = '#list-gallery-item' + actualImage.toString();
+    if (!blockScrolling) {
+        if (actualImage > 0 && actualImage < 7) {
+            actualImage += 1;
+            window.location.href = '#list-gallery-item' + actualImage.toString();
+        }
+        blockScrolling = true;
+
+        setTimeout(() => {
+            blockScrolling = false;
+        }, 500);
     }
+    
 
 }
 
 function minusImage() {
-    if (actualImage > 1 && actualImage < 8) {
-        actualImage -= 1;
-        window.location.href = '#list-gallery-item' + actualImage.toString();
+    if (!blockScrolling) {
+        if (actualImage > 1 && actualImage < 8) {
+            actualImage -= 1;
+            window.location.href = '#list-gallery-item' + actualImage.toString();
+        }
+        blockScrolling = true;
+
+        setTimeout(() => {
+            blockScrolling = false;
+        }, 500);
     }
 
 }
